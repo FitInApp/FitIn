179ea184660675400a2f1903249148af67c97490
Ailany Rodriguez <arodr1293@fiu.edu>
2023-03-24 12:24:05 -0700

    Initial Commit

---
+++ FitIn/FitInApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitInApp.swift
+//  FitIn
+//
+//  Created by Ailany Rodriguez on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FitInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
