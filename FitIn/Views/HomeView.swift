919040de51923b0c24c036e6c0d26eeceb54d50f
Malcolm <verdier_m@icloud.com>
2023-04-01 22:03:44 -0400

    add post component and actions

--- FitIn/Views/HomeView.swift
+++ FitIn/Views/HomeView.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
-
+import ParseSwift
 struct HomeView: View {
+    @State private var newPost = Post()
+    @State private var weight = "100"
+    @State private var calories = "2000"
+    @Environment(\.colorScheme) var colorScheme
+    func addPost() {
+        print("Add Post Entry")
+        let currentDateTime = Date()
+        
+        newPost.createObject(user: User.current!, imageID: URL(string:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0r0JFlxQy2lhU9rRAUhHrS&ust=1680486652876000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJCwquGKiv4CFQAAAAAdAAAAABAE")!, date: currentDateTime, weight: weight, calories: calories)
+        
+    }
     var body: some View {
-        Text("Home")
+        VStack{
+            Button(action: {addPost()
+                
+            }) {
+                HStack {
+                    Text("Add Post").foregroundColor(.white).padding()
+                }
+            }.background(Color.black).cornerRadius(30)
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<10) {
+                        Text("Item \($0)")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .frame(width: 300, height: 200)
+                            .background(.red)
+                    }
+                }
+            }
+        }
+        
     }
 }
 
