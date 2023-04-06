47963f65dfe21e2c7dc40ed091cac1e836863f2a
Ailany Rodriguez <arodr1293@fiu.edu>
2023-04-06 13:55:50 -0700

    Initialize helper functions & progress screen

---
+++ FitIn/Models/WorkoutLog.swift
@@ -0,0 +1,25 @@
+//
+//  ExerciseLog.swift
+//  FitIn
+//
+//  Created by Ailany Rodriguez on 3/31/23.
+//
+
+import Foundation
+import ParseSwift
+
+struct WorkoutLog : ParseObject {
+    var originalData: Data?
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseSwift.ParseACL?
+    
+    var exercise:Exercise?
+    var duration:Float?
+    var weight:Float?
+    var repCount:Int?
+    var setCount:Int?
+    var caloriesBurned:Int?
+}
+
--- FitIn/Views/TrackerView.swift
+++ FitIn/Views/TrackerView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct TrackerView: View {
+    
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        Text("Tracker")
+        
+        VStack {
+            HStack{
+                Text("Progress".uppercased())
+                    .font(.custom("AllertaStencil-Regular", size: 40))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Button("+ Add"){
+                    onAddButtonTapped()
+                }
+                .font(.subheadline)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 8)
+                        .foregroundColor(Color(.systemBackground))
+                        .background(Color(.label))
+                        .cornerRadius(8)
+            }.padding([.leading,.trailing], 16)
+            
+            Spacer()
+        }
+        
+    }
+    
+    
+    func onAddButtonTapped () {
+        //
+    }
+    func addWorkout () {
+        //TODO: implement add workout using Exercise object and add to a new object that holds historical information
+    }
+}
+
+struct AddWorkoutView: View {
+    var body: some View{
+        VStack{
+            
+        }
     }
 }
 
